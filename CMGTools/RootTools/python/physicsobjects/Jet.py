36f5ba720cd358d204c257977e7f1885d7ab0faa
Yuta <Yuta.Takahashi@cern.ch>
2014-10-29 12:25:56 +0100

    update physicsobjects to be the old one

--- CMGTools/RootTools/python/physicsobjects/Jet.py
+++ CMGTools/RootTools/python/physicsobjects/Jet.py
@@ -4,7 +4,7 @@ loose_WP = [
     (0, 2.5, -0.8),
     (2.5, 2.75, -0.74),
     (2.75, 3.0, -0.68),
-    (3.0, 5.2, -0.77),
+    (3.0, 5.0, -0.77),
     ]
 
 # Working point 2 May 2013 (Phil via H2tau list)
@@ -12,24 +12,29 @@ loose_53X_WP = [
     (0, 2.5, -0.63),
     (2.5, 2.75, -0.60),
     (2.75, 3.0, -0.55),
-    (3.0, 5.2, -0.45),
+    (3.0, 5.0, -0.45),
     ]
 
 
 class Jet(PhysicsObject):
 
-    def jetID(self,name=""):
-        if name == "POG_PFID":
-            if   self.jetID("POG_PFID_Tight")  : return 3;
-            elif self.jetID("POG_PFID_Medium") : return 2;
-            elif self.jetID("POG_PFID_Loose")  : return 1;
-            else                               : return 0;
-        else:
-            return self.physObj.jetID(name)
-
     def looseJetId(self):
-        '''PF Jet ID (loose operation point) [method provided for convenience only]'''
-        return self.jetID("POG_PFID_Loose")
+        '''PF Jet ID
+        '''
+        pfJetIdPassed = ( abs(self.eta()) <= 2.4                                             and 
+                              (self.component(1).fraction()                                > 0    and
+                               self.component(2).fraction()                                < 0.99 and 
+                               self.component(4).fraction()                                < 0.99 and 
+                               self.component(5).fraction() < 0.99 and 
+                               self.component(1).number()                                  > 0    and
+                               self.nConstituents()                                        > 1   )or
+                              abs(self.eta()) > 2.4                                               and
+                              (self.component(4).fraction()                                < 0.99 and 
+                               self.component(5).fraction() < 0.99 and 
+                               self.nConstituents()                                        > 1    ) 
+                            ) 
+        #return self.getSelection('cuts_looseJetId') 
+        return pfJetIdPassed 
 
     def puJetId(self, wp53x=False):
         '''Full mva PU jet id (default: old loose working point used by Josh)'''
