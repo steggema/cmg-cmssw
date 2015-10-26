d3e94b86c3affe5706d1f6cd4bf18e2a23cecf25
Jan Steggemann <jan.steggemann@cern.ch>
2015-10-26 18:41:57 +0100

    Update triggers

--- CMGTools/H2TauTau/python/proto/samples/phys14/triggers_tauMu.py
+++ CMGTools/H2TauTau/python/proto/samples/phys14/triggers_tauMu.py
@@ -1,3 +1,5 @@
+# PHYS14
+
 data_triggers = [
     ]
 
--- CMGTools/H2TauTau/python/proto/samples/spring15/triggers_muEle.py
+++ CMGTools/H2TauTau/python/proto/samples/spring15/triggers_muEle.py
@@ -1,4 +1,10 @@
 data_triggers = [
+    # 2015D
+    'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v3',
+    'HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v3',
+    # 2015C
+    'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v2',
+    'HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v2',
     ]
 
 
@@ -6,6 +12,14 @@ mc_triggers = [
     'HLT_Mu23_TrkIsoVVL_Ele12_CaloIdL_TrackIdL_IsoVL_v1',
     'HLT_Mu8_TrkIsoVVL_Ele23_CaloIdL_TrackIdL_IsoVL_v1'
     ]
+
+mc_triggerfilters = [
+    'hltMu23TrkIsoVVLEle12CaloIdLTrackIdLIsoVLMuonlegL3IsoFiltered23',
+    'hltMu8TrkIsoVVLEle23CaloIdLTrackIdLIsoVLMuonlegL3IsoFiltered8',
+
+    'hltMu23TrkIsoVVLEle12CaloIdLTrackIdLIsoVLElectronlegTrackIsoFilter',
+    'hltMu8TrkIsoVVLEle23CaloIdLTrackIdLIsoVLElectronlegTrackIsoFilter'
+]
 
 embed_triggers = [
     ]
