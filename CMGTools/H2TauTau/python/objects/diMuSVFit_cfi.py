929dcac0f5eedda3b0c217bac64f8df95317f287
Jan Steggemann <jan.steggemann@cern.ch>
2015-11-04 13:36:07 +0100

    Switch default SVFit method to MCMC

--- CMGTools/H2TauTau/python/objects/diMuSVFit_cfi.py
+++ CMGTools/H2TauTau/python/objects/diMuSVFit_cfi.py
@@ -4,6 +4,6 @@ diMuSVFit = cms.EDProducer(
     "DiMuWithSVFitProducer",
     diTauSrc = cms.InputTag("cmgDiMuTauPtSel"),
     SVFitVersion =  cms.int32(2), # 1 for 2011 version, 2 for new 2012 (slow) version
-    fitAlgo = cms.string('VEGAS'),
+    fitAlgo = cms.string('MC'),
     verbose = cms.untracked.bool(False)
     )
